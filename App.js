a955e9d01916091853b0ce375e61ffc52284e37a
priya <absapriya@gmail.com>
2023-12-21 11:22:54 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", { id: "heading",xyz:"abc"}, "Hello from React");
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
- /* <div id='parent'>
-    <div id='child1'>
-        <h1>I am h1</h1>
-        <h2>I am h2</h2>
-    </div>
-    <div id='child2'>
-        <h1>I am h1</h1>
-        <h2>I am h2</h2>
-    </div>
-</div> */
+import { FaUser } from "react-icons/fa";
+import logoImage from './images/logo.jpg';
+const HeaderComponent=()=>
+{
+    return(
+        
+        <header style={{backgroundColor:"aliceblue"}}>
+            <div className="flex-container">
+                <div><img src={logoImage} height={90} width={100} alt="logo"/></div>
+                <div className="search">
+                   <div > <input className="search-box"type="text"></input></div>
+                    <div><button className="submit-button" type="submit">Search</button></div>
+                    </div>
+                <div className="usericon"><FaUser /></div>
+                   </div>             
+            
+        </header>
+    )
+}
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I amh1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I amh1"),
-    React.createElement("h2", {}, "I am h2"),
-  ]),
-]);
+
+
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'))
+root.render(<HeaderComponent/>)
